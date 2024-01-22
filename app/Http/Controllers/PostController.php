fa8dd044412a399da6a591cdec0b42124ca08904
Fauzan Agra <fauzanagra1@gmail.com>
2024-01-23 03:36:00 +0700

    [CodePolitan][8.4] FullStack Laravel 9 - Menambahkan fitur mengirimkan Notifikasi ke pada user melalui mailtraip

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\BlogPosted;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 use function PHPUnit\Framework\returnSelf;
@@ -58,10 +60,12 @@ class PostController extends Controller
         $title = $request->input('title');
         $content = $request->input('content');
 
-        Post::create([
+        $post = Post::create([
             'title' => $title,
             'content' => $content
         ]);
+
+        Mail::to(Auth::user()->email)->send(new BlogPosted($post));
 
         return redirect('posts');
     }
---
+++ resources/views/mails/blogPosted.blade.php
@@ -0,0 +1,15 @@
+<div style="width: 100%">
+    Ada Postingan baru nih yang ada di Blog kamu
+    <div
+        style="
+        width: 100%; 
+        box-shadow: 1px 1px 3px lightgrey; 
+        padding: 5px 20px; 
+        margin-top: 10px; 
+        font-style: italic; 
+        border-radius: 10px;">
+        <h3>{{ $post->title }}</h3>
+        <h3>{{ $post->content }}</h3>
+
+    </div>
+</div>
